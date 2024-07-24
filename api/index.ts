ffb94102a7dda21ff7b5ad3292011e1d4ef87cb2
Aissamdev2 <shiranui201103@gmail.com>
2024-07-24 20:17:14 +0200

    error2

--- api/index.ts
+++ api/index.ts
@@ -13,8 +13,8 @@ import path from 'path';
 import cors from 'cors';
 
 const app = express();
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
 
 const prisma = new PrismaClient();
 
